a5934d73c4bb50b4b48c72ed2a5f633d327cb7cb
Qianxun Chen <qianxun.chen@zkm.de>
2026-05-04 21:02:05 +0200

    add week4

---
+++ week4/assignment4.md
@@ -0,0 +1,13 @@
+## Assignment 3
+**DUE 11 May Monday Noon**
+
+This week we want to make some generative art using Python!
+
+- Your code should include at least one set of nested loops and one conditional statement
+- Use `random` and list together to introduce some randomness
+- Feel free to find another drawing to recreate in code. In such case, provide a link of your reference image in comment
+- Save your file as `generative_art.py` inside a week3 folder!
+
+Bonus
+- Introduce color (palette) into your drawing
+- Integrate other methods of turtle from [official documentation](https://docs.python.org/3.12/library/turtle.html)
---
+++ week4/turtle_template.py
@@ -0,0 +1,64 @@
+### Basic Turtle cheatsheet
+
+# import turtle
+from turtle import *
+
+# initial setup: canvas size
+width = 400
+height = 400
+setup(width, height)
+
+# turn off animation, comment it out to see the drawing process
+tracer(0, 0)
+# set background color
+bgcolor('white') # You can either use color names
+# change the color of the lines
+color('#000') # Or Hex Code
+
+# draw rotating lines
+for i in range(4):
+    # always move to the same point
+    penup()
+    goto(100, -100) # lower right corner
+    pendown()
+    right(20)
+    forward(50)
+    backward(100) # add this to make the lines cross in center
+
+# after rotation, remember to rotate back
+# to the default direction: East (to the right)
+left(80)
+
+# move to lower left corner
+penup()
+goto(-150,-150)
+pendown()
+
+# draw triangle
+for i in range(3):
+    forward(100) # draw from starting point, go forward
+    left(120) # turn right 90 degree
+
+# move to upper left corner
+penup()
+goto(-180,180)
+pendown()
+
+# draw square
+for i in range(4):
+    forward(100) # draw from starting point, go forward
+    right(90) # turn right 90 degree
+
+# move to the center
+penup()
+goto(0,0)
+pendown()
+
+# draw circle
+fillcolor("blue")
+begin_fill()
+circle(50, 360) # radius & angle
+end_fill()
+
+# quite with click
+exitonclick()
